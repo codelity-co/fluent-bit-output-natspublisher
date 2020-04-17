3060f066c0a4497ce0e3427af3d4b148da3ea363
reidlai <reidlai@gmail.com>
2020-04-17 09:34:05 +0800

    add stan connection

--- main.go
+++ main.go
@@ -9,8 +9,9 @@ import (
 	"strings"
 	"unsafe"
 
-	"github.com/fluent/fluent-bit-go/output"
 	nats "github.com/nats-io/nats.go"
+	stan "github.com/nats-io/stan.go"
+	"github.com/fluent/fluent-bit-go/output"
 
 	"github.com/codelity-co/fluentbit-plugin-natspublisher/pkg/natspublisher"
 )
@@ -27,12 +28,19 @@ func FLBPluginRegister(ctx unsafe.Pointer) int {
 //export FLBPluginInit
 func FLBPluginInit(ctx unsafe.Pointer) int {
 	pluginConfig := &natspublisher.PluginConfig{
-		ServerUrls: output.FLBPluginConfigKey(ctx, "ServerUrls"),
-		Debug: output.FLBPluginConfigKey(ctx, "Debug"),
+		ServerUrls:      output.FLBPluginConfigKey(ctx, "ServerUrls"),
+		EnableStreaming: output.FLBPluginConfigKey(ctx, "EnableStreaming"),
+		ClusterId:       output.FLBPluginConfigKey(ctx, "ClusterId"),
+		ChannelId:       output.FLBPluginConfigKey(ctx, "ChannelId"),
+		Debug:           output.FLBPluginConfigKey(ctx, "Debug"),
 	}
 
 	if pluginConfig.ServerUrls == "" {
 		pluginConfig.ServerUrls = "nats://localhost:4222"
+	}
+
+	if pluginConfig.EnableStreaming == "" {
+		pluginConfig.EnableStreaming = "off"
 	}
 
 	if pluginConfig.Debug == "" {
@@ -61,16 +69,20 @@ func FLBPluginFlushCtx(ctx, data unsafe.Pointer, length C.int, _ *C.char) int {
 
 	for {
 
+		// Receive records from fluent-bit filter plugin
 		ret, _, record := output.GetRecord(decoder)
 		if ret != 0 {
 			break
 		}
 		plugin.Logger.Debug(fmt.Sprintf("record = %v", record))
 
+		// Determine NATS topic
 		var subject string = string(record["topic"].([]uint8))
 		subject = strings.ReplaceAll(subject, "/", ".")
 		delete(record, "topic")
+		plugin.Logger.Debug(fmt.Sprintf("subject = %v", subject))
 
+		// Create NATS payload
 		payload := make(map[string]interface{})
 		for k, v := range record {
 			payload[k.(string)] = v
@@ -79,16 +91,30 @@ func FLBPluginFlushCtx(ctx, data unsafe.Pointer, length C.int, _ *C.char) int {
 		jsonPayload, err := json.Marshal(payload)
 		if err != nil {
 			plugin.Logger.Error(err)
-		} else {
-			msg := &nats.Msg{
-				Subject: subject,
-				Data:    jsonPayload,
-			}
-			plugin.Logger.Debug(fmt.Sprintf("msg = %v", msg))
-			if err = plugin.Conn.PublishMsg(msg); err != nil {
+			continue
+		}
+		plugin.Logger.Debug(fmt.Sprintf("jsonPayload = %v", string(jsonPayload)))
+
+		// Publish NATS message
+		if plugin.StanConn != nil {
+			var sc stan.Conn = *plugin.StanConn
+			_, err = sc.PublishAsync(subject, jsonPayload, func(ackedNuid string, err error) {
+				if err != nil {
+					plugin.Logger.Error(err)
+				}
+				plugin.Logger.Debug(ackedNuid)
+			})
+			if err != nil {
 				plugin.Logger.Error(err)
 				return output.FLB_ERROR
 			}
+		} else {
+			var nc *nats.Conn = plugin.Conn
+			if err = nc.Publish(subject, jsonPayload); err != nil {
+				plugin.Logger.Error(err)
+				return output.FLB_ERROR
+			}
+
 		}
 
 	}
