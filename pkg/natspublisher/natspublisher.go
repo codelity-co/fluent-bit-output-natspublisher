7e96c7382c7a3d00bd01a6d07d7d16b4d12a3dc8
reidlai <reidlai@gmail.com>
2020-04-17 09:34:32 +0800

    use milliseconds instead of nano for database compatibility

--- pkg/natspublisher/natspublisher.go
+++ pkg/natspublisher/natspublisher.go
@@ -2,16 +2,21 @@
 package natspublisher
 
 import (
+	"fmt"
 	"os"
 	"strings"
 	"time"
 
 	nats "github.com/nats-io/nats.go"
+	stan "github.com/nats-io/stan.go"
 	"github.com/sirupsen/logrus"
 )
 
 type PluginConfig struct {
 	ServerUrls string
+	EnableStreaming string
+	ClusterId string
+	ChannelId string
 	Debug string
 }
 
@@ -19,6 +24,7 @@ type NatsPublisher struct {
 	Config *PluginConfig
 	Logger *logrus.Logger
 	Conn *nats.Conn
+	StanConn *stan.Conn
 }
 
 func NewPlugin(config *PluginConfig) (*NatsPublisher, error) {
@@ -28,10 +34,28 @@ func NewPlugin(config *PluginConfig) (*NatsPublisher, error) {
 	opts.AllowReconnect = true
 	opts.ReconnectWait = time.Duration(1*time.Second)
 	opts.MaxReconnect=-1
-	conn, err := opts.Connect()
+
+	nc, err := opts.Connect()
 	if(err != nil) {
 		return nil, err
 	}
+
+	var sc stan.Conn
+	if strings.ToLower(config.EnableStreaming) == "on" || strings.ToLower(config.EnableStreaming) == "true"  {
+		if len(config.ClusterId) == 0 {
+			return nil, fmt.Errorf("ClusterId cannot be empty.")
+		}
+		
+		if len(config.ChannelId) == 0 {
+			return nil, fmt.Errorf("ClusterId cannot be empty.")
+		}
+
+		sc, err = stan.Connect(config.ClusterId, config.ChannelId, stan.NatsConn(nc))
+		if err != nil {
+			return nil, fmt.Errorf("Cannot connect NATS Streaming cluster %v with channel %v", config.ChannelId, config.ChannelId)
+		}
+	}
+
 
 	logger := logrus.New()
 	logger.SetLevel(logrus.InfoLevel)
@@ -39,10 +63,10 @@ func NewPlugin(config *PluginConfig) (*NatsPublisher, error) {
 	if strings.ToLower(config.Debug) == "on" || strings.ToLower(config.Debug) == "true"  {
 		logger.SetLevel(logrus.DebugLevel)
 	}
-
 	return &NatsPublisher{
 		Config: config,
 		Logger: logger,
-		Conn: conn,
+		Conn: nc,
+		StanConn: &sc,
 	}, nil
 }
